6c66fbd6c1ab02aeea0f405b3665c3cd536a5bc3
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-05-31 14:31:54 -0700

    update files

---
+++ sql-scripts/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title,url,description)
+VALUES 
+('W3 SCHOOL', 'https://www.w3schools.com/', `THE WORLD'S LARGEST WEB DEVELOPER SITE`),
+('THINKFUL', 'https://www.thinkful.com/' , 'Coding Bootcamp'),
+('MDN WEB DOCS', 'https://developer.mozilla.org/en-US/', 'Resources for Developers, by Developers'),
+('THE MODERN JS TUTORIAL', 'https://javascript.info/', 'from the basics to advanced topics with simple but detail explainations'),
+('NODE DOCS', 'https://nodejs.org/en/docs/', 'Provide detailed info about function and object in Node'),
+('JAVA T POINT', 'https://www.javatpoint.com/', 'The best portal to learn technology'),
+('REACT DOCS', 'https://reactjs.org/docs/getting-started.html', 'React docs and related resources'),
+('POSTGRESQL', 'https://www.postgresql.org/docs/12/index.html', 'PostgreSQL documentation'),
+('DBEAVER', 'https://dbeaver.com/docs/wiki/', 'About DBeaver'),
+('DATAGRIP', 'https://www.jetbrains.com/datagrip/', 'Many Databases, one tool');
